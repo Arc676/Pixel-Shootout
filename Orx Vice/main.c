50eb241ca6f260ca206afced35785029c2876d18
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-09-18 20:16:22 +0800

    Initial Commit

---
+++ Orx Vice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Orx Vice
+//
+//  Created by Alessandro Vinciguerra on 18/09/2017.
+//  Copyright © 2017 Arc676. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
