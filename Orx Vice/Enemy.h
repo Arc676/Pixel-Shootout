b15d34419e3819a3ae05b24d2e5f48df6b68e8cb
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-09-25 19:00:24 +0800

    Added Enemy
    Created INI and class
    No functionality yet

---
+++ Orx Vice/Enemy.h
@@ -0,0 +1,31 @@
+//
+//  Enemy.hpp
+//  Orx Vice
+//
+//  Created by Alessandro Vinciguerra on 25/09/2017.
+//      <alesvinciguerra@gmail.com>
+//Copyright (C) 2017 Arc676/Alessandro Vinciguerra
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation (version 3)
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//See README and LICENSE for more details
+
+#ifndef Enemy_h
+#define Enemy_h
+
+#include "Entity.h"
+
+class Enemy : public Entity {
+public:
+};
+
+#endif
