72ac7d6f7dee32c2d3598ee0514a441b443095ec
Arc676/Alessandro <alesvinciguerra@gmail.com>
2017-10-29 19:02:11 +0800

    Better death screen
    Added You Died image (red text)
    Moved death screen to Entities.ini

--- Orx Vice/StandAlone.cpp
+++ Orx Vice/StandAlone.cpp
@@ -41,7 +41,6 @@ StandAlone::StandAlone() {}
 orxSTATUS orxFASTCALL StandAlone::Init() {
 	orxViewport_CreateFromConfig("Viewport");
 	orxConfig_Load("StaticScene.ini");
-	orxConfig_Load("Died.ini");
 	orxObject_CreateFromConfig("Scene");
 
 	orxConfig_Load("Entities.ini");
